39e5d9c95b03bed94d33c890ae38d76fdb503274
valeiramain <valentinairamain2020@gmail.com>
2026-01-14 16:58:56 -0300

    se maqueta formulario para ingresar y mostrar el color

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import FormularioColor from './components/FormularioColor.jsx';  
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import FormularioColor from "./components/FormularioColor.jsx";
 
 function App() {
- 
-
   return (
-    <>
-     <h1>Inicio</h1>
-     <FormularioColor />
-    </>
-  )
+    <main className="container py-4">
+      <h1 className="text-center text-light display-5 mb-4">
+        Paleta de Colores
+      </h1>
+      <FormularioColor />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -1,15 +1,61 @@
-
-import PaletaColores from './PaletaColores.jsx';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import PaletaColores from "./PaletaColores.jsx";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const FormularioColor = () => {
-   
-    return (
-        <>
-            <h1>Formulario color</h1>
-            <PaletaColores />
-        </>
+  const [color, setColor] = useState("");
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-    );
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        {/* Contenedor principal: se vuelve flex a partir de md */}
+        <div className="d-md-flex gap-2 align-items-md-center">
+          <Form.Group
+            className="mb-3 mb-md-0 d-flex gap-2 flex-grow-1 align-items-center"
+            controlId="formBasicColor"
+          >
+            <div
+              className="border cuadrado"
+              style={{ backgroundColor: color || "transparent" }}
+            ></div>
+            <Form.Control
+              type="text"
+              onChange={(e) => setColor(e.target.value)} value={color}
+              placeholder="Color en inglés, hexadecimal ó RGB/RGBA"
+              {...register("color", {
+                required: "El color es un dato obligatorio",
+                minLength: {
+                  value: 5,
+                  message: "El color debe contener como minimo 3 caracteres",
+                },
+                maxLength: {
+                  value: 20,
+                  message: "El color debe contener como maximo 20 caracteres",
+                },
+              })}
+            />
+
+            <Button variant="primary" type="submit">
+              Enviar
+            </Button>
+          </Form.Group>
+        </div>
+      </Form>
+      <PaletaColores></PaletaColores>
+    </>
+  );
 };
 
 export default FormularioColor;
